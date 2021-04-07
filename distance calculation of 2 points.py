7a25681e26c4070f13bc4c7d4ea9b1f064265716
Teekam Chand Khandelwal <teekamkhandelwal@gmail.com>
2021-04-07 16:05:58 +0530

    Delete distance calculation of 2 points.py

--- distance calculation of 2 points.py
+++
@@ -1,9 +0,0 @@
-print('pont1')
-x1=eval(input('enter x1 coordinate'))
-y1=eval(input('enter y1 coordinate'))
-print('point2')
-x2=eval(input('enter x2 coordinate'))
-y2=eval(input('enter y2 coordinate'))
-l1=(x2-x1)**2+(x1-y1)**2
-d=l1**0.5
-print('distance=',d)
